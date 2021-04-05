fe261be76ac0e4f48c0360a181b859902f35e095
Mark Wlodawski <imakethesites@gmail.com>
2021-04-04 21:23:25 -0400

    Added JSON server for data loading

--- src/views/jobs/Jobs.vue
+++ src/views/jobs/Jobs.vue
@@ -1,9 +1,14 @@
 <template>
   <h1>Jobs</h1>
-  <div v-for="job in jobs" :key="job.id" class="job">
-    <router-link :to="{ name: 'JobDetails', params: { id: job.id } }">
-      <h2>{{ job.title }}</h2>
-    </router-link>
+  <div v-if="jobs.length">
+    <div v-for="job in jobs" :key="job.id" class="job">
+      <router-link :to="{ name: 'JobDetails', params: { id: job.id } }">
+        <h2>{{ job.title }}</h2>
+      </router-link>
+    </div>
+  </div>
+  <div v-else>
+    <p>Loading jobs...</p>
   </div>
 </template>
 
@@ -11,20 +16,14 @@
 export default {
   data() {
     return {
-      jobs: [
-        { title: 'MQ Exports SEO Manager', id: 1, details: 'Manages the SEO' },
-        {
-          title: 'MQ Exports Social Media Manager',
-          id: 2,
-          details: 'Manages the Social Media Presence',
-        },
-        {
-          title: 'MQ Exports App Developer',
-          id: 3,
-          details: 'Develops the mobile application',
-        },
-      ],
+      jobs: [],
     };
+  },
+  mounted() {
+    fetch('http://localhost:3000/jobs')
+      .then((res) => res.json())
+      .then((data) => (this.jobs = data))
+      .catch((err) => console.log(err.message));
   },
 };
 </script>
